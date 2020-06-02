8c886f4886427026d7f25ee0b0e371468a2aa6f1
unknown <gmateu@esliceu.com>
2020-06-02 18:04:17 +0200

    rutes i rutes a un fitxer dart

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
  
-import 'package:componentes/src/pages/home_page.dart';
 
  
 void main() => runApp(MyApp());
@@ -15,10 +14,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       //home: HomePage(),
       initialRoute: '/',
-      routes: <String,WidgetBuilder>{
-        '/'      : (BuildContext context) => HomePage(),
-        'alert'  : (BuildContext context) => AlertPage(),
-        'avatar' : (BuildContext context) => AvatarPage(),
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (RouteSettings settings){
+        print("ruta cridada: ${ settings.name }");
+
+        return MaterialPageRoute(
+          builder: (BuildContext context) => AlertPage()
+        );
       },
       );
   }
